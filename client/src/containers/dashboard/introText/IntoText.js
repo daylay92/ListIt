0df4cc4beb1792251d85afdec29803940df30454
Ayodele <daylay92@yahoo.com>
2019-10-25 04:02:36 +0100

    feature(list-form) add dynamic name display

--- client/src/containers/dashboard/introText/IntoText.js
+++ client/src/containers/dashboard/introText/IntoText.js
@@ -5,7 +5,7 @@ const IntroText = props => {
   return (
     <div className={classes.introText}>
       <p className={classes.introText_hero}>
-        Hello Ayodele, You currently do not have a bucketlist
+        Hello {props.firstName}, You currently do not have a bucketlist
       </p>
       <p className={classes.introText_caption}>
         Dare to dream by adding some goals today, You never know where they would lead you.
